5068026166304acf135d146a433e19025fa53ea3
alialaba <alialaba079@gmail.com>
2020-12-19 23:33:19 -0800

    i addded more functunalities(to allow user search if we have an item in store or not) to js and also added a json file

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@
 
 
  const loadData = () => {
-     const API = 'https://fakestoreapi.com/products';
+     const API = '../data/stores.json';
      fetch(API)
          .then((res) => {
              if (true) {
@@ -22,24 +22,24 @@
              data.forEach((item) => {
                  const { image, title, price, category } = item;
                  output += `
-                 <div class="product">
-                 <div>
-                 <img class="product-image" src="${image}" >
-                 </div>
-                 <ul class="product-lists">
-                 <li>${title}</li>
-                 <div class="flex-product">
-                 <li>$${price}</li>
-                 <li><i class="fas fa-plus"></i></li>
-                 </div>
+                  <div class="product">
+                  <div>
+                  <img class="product-image" src="${image}" >
+                  </div>
+                  <ul class="product-lists">
+                  <li>${title}</li>
+                  <div class="flex-product">
+                  <li>$${price}</li>
+                  <li><i class="fas fa-plus"></i></li>
+                  </div>
 
-                 </ul>
-
-
-                 </div>
+                  </ul>
 
 
-                 `
+                  </div>
+
+
+                  `
                      // console.log(item)
                  displayData(output)
              })
@@ -56,12 +56,12 @@
 
  //making some search
  const searchInput = document.getElementById('search');
- const matchInput = document.getElementById('match-products');
+ const matchInput = document.querySelector('.products');
  //search items.json and filter
  const searchItems = async searchText => {
-         const res = await fetch('https://fakestoreapi.com/products');
+         const res = await fetch("../data/stores.json");
          const items = await res.json();
-         //  console.log(items)
+
          //get matches to current input 
          let matches = items.filter(item => {
              const regex = new RegExp(`^${searchText}`, 'gi');
@@ -70,17 +70,34 @@
          })
          if (searchText.length === 0) {
              matches = [];
+             matchInput.innerHTML = loadData();;
          }
-         //  console.log(matches)
 
+         outputHtml(matches);
 
      }
      //displaying search items
  const outputHtml = matches => {
      if (matches.length > 0) {
+         const html = matches.map(match => `
+         <div class="product">
+                   <div>
+                  <img class="product-image" src="${match.image}" >
+                  </div>
+                 <ul class="product-lists">
+                 <li>${match.title}</li>
+               <div class="flex-product">
+                <li>$${match.price}</li>
+                <li><i class="fas fa-plus"></i></li>
+                  </div>
+                 </ul>
+              </div>
+         
+         `).join('');
+         matchInput.innerHTML = html;
 
-
+     } else {
+         matchInput.innerHTML = `<h3 style="color:#fff;">Sorry, No Products Matched Your Search</h3>`
      }
-
  }
- searchInput.addEventListener('input', () => searchItems(search.value))
+ searchInput.addEventListener('input', () => searchItems(search.value));
